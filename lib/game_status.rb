2994026a890333221e50a2551bb77cd955e1b9cb
Qoosim <plenty4luv@yahoo.com>
2020-01-04 23:54:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,8 +51,7 @@ def winner(board)
   index = won?(board)
   if index == false
     return nil
-  end
-  if board[index[0]] == "X"
+  elsif board[index[0]] == "X"
     return "X"
   else
     return "O"
