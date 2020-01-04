a63b8f8c3df6c80141cf4c838750a788d3cf31be
Qoosim <plenty4luv@yahoo.com>
2020-01-04 21:46:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,11 +21,11 @@ def won?(board)
     else
       return false
     end
-
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return win_combinations
-    else
-      return false
-    end
+    #
+    # if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    #   return win_combinations
+    # else
+    #   return false
+    # end
   end
 end
