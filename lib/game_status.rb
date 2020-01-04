4caf3e84dadf08aa34c81575888e76d20024bc5d
Qoosim <plenty4luv@yahoo.com>
2020-01-04 22:47:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,23 +13,16 @@ def won?(board)
     win_index_2 = win_combinations[1]
     win_index_3 = win_combinations[2]
 
-    if position_taken?(board, win_index_1) == "X" && position_taken?(board, win_index_2) == "X" && position_taken?(board, win_index_3) == "X"
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X")
       return win_combinations
-    elsif position_taken?(board, win_index_1) == "O" && position_taken?(board, win_index_2) == "O" && position_taken?(board, win_index_3)  == "O"
+    elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combinations
     else
       return false
     end
-    # position_1 = board[win_index_1]
-    # position_2 = board[win_index_2]
-    # position_3 = board[win_index_3]
-    #
-    # if (position_1 == "X" && position_2 == "X" && position_3 == "X")
-    #   return win_combinations
-    # elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-    #   return win_combinations
-    # else
-    #   return false
-    # end
   end
 end
